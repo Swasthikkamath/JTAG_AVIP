9d040923081a8c846590b23924c7edd00acc8732
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-03-10 15:41:03 +0530

    Update JtagSlaveDriverProxy.sv

--- src/hvlTop/jtagSlaveAgent/JtagSlaveDriverProxy.sv
+++ src/hvlTop/jtagSlaveAgent/JtagSlaveDriverProxy.sv
@@ -3,7 +3,8 @@
 
 class JtagSlaveDriver extends uvm_driver#(JtagSlaveTransaction);
   `uvm_component_utils(JtagSlaveDriver)
-
+  virtual JtagSlaveDriverBfm jtagSlaveDriverBfm;
+  JtagSlaveAgentConfig jtagSlaveAgentConfig;
   extern function new (string name = "JtagSlaveDriver", uvm_component parent);
   extern virtual function void build_phase(uvm_phase phase);
   extern virtual task run_phase(uvm_phase phase);
@@ -19,8 +20,8 @@ function JtagSlaveDriver :: build_phase(uvm_phase phase);
   if(!(uvm_config_db #(JtagSlaveAgentConfig) :: get(this,"","jtagSlaveAgentConfig",jtagSlaveAgentConfig)))
     `uvm_fatal(get_type_name(),"FAILED TO GET CONFIG IN Slave DRIVER")
 
-  if(!(uvm_config_db #(JtagSlaveBfm) :: get(this,"","jtagSlaveBfm",jtagSlaveBfm)))
-    `uvm_fatal(get_type_name(),"FAILED TO GET VIRTUAL POINTER TO Slave BFM IN Slave DRIVER")
+    if(!(uvm_config_db #(JtagSlaveDriverBfm) :: get(this,"","jtagSlaveDriverBfm",jtagSlaveDriverBfm)))
+      `uvm_fatal(get_type_name(),"FAILED TO GET VIRTUAL POINTER TO Slave DRIVERBFM IN Slave DRIVER")
 endfunction : build_phase
 
 task JtagSlaveDriver :: run_phase(uvm_phase phase);
