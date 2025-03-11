aaa09ef8bc3fe826e098608191a48b31cb1b06f6
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-03-12 00:38:27 +0530

    Update JtagMasterAgentBfm.sv

--- src/hdlTop/jtagMasterAgentBfm/JtagMasterAgentBfm.sv
+++ src/hdlTop/jtagMasterAgentBfm/JtagMasterAgentBfm.sv
@@ -41,4 +41,4 @@ module JtagMasterAgentBfm(JtagIf jtagIf);
 
 //  bind jtagMasterMonitorBfm jtagMasterAssertions TestVectrorTestingAssertions();
 
-endmodule : jtagMasterAgentBfm
+endmodule : JtagMasterAgentBfm
