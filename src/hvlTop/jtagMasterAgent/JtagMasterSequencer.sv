f5a71ca826fe8d24cfe273df2cde27ddab0f0eec
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-03-12 00:48:03 +0530

    Update JtagMasterSequencer.sv

--- src/hvlTop/jtagMasterAgent/JtagMasterSequencer.sv
+++ src/hvlTop/jtagMasterAgent/JtagMasterSequencer.sv
@@ -1,7 +1,7 @@
 `ifndef JTAGMASTERSEQUENCER_INCLUDED_
 `define JTAGMASTERSEQUENCER_INCLUDED_
 
-class JtagMasterSequencer extends uvm_sequencer #(JtagMasterSequencer);
+class JtagMasterSequencer extends uvm_sequencer #(JtagMasterTransaction);
   `uvm_component_utils(JtagMasterSequencer)
 
   extern function new(string name = "JtagMasterSequencer",uvm_component parent);
