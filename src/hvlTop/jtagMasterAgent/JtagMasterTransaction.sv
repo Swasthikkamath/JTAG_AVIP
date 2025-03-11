6e81fae2a113cf021aa728c584c731c3d8e4725b
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-03-12 01:17:40 +0530

    Update JtagMasterTransaction.sv

--- src/hvlTop/jtagMasterAgent/JtagMasterTransaction.sv
+++ src/hvlTop/jtagMasterAgent/JtagMasterTransaction.sv
@@ -5,7 +5,7 @@ class JtagMasterTransaction extends uvm_sequence_item;
 
   `uvm_object_utils(JtagMasterTransaction)
 
-  logic[31:0]jtagTestVector;
+  rand logic[31:0]jtagTestVector;
   extern function new(string name = "JtagMasterTransaction");
   extern function void do_copy(uvm_object rhs);
   extern function bit do_compare(uvm_object rhs , uvm_comparer comparer = null);
