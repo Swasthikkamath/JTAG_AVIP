ac3b1a3596998c5761f602cb3424a572218cd4e5
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-03-12 01:25:10 +0530

    Update JtagMasterAgent.sv

--- src/hvlTop/jtagMasterAgent/JtagMasterAgent.sv
+++ src/hvlTop/jtagMasterAgent/JtagMasterAgent.sv
@@ -35,6 +35,8 @@ function void  JtagMasterAgent ::build_phase (uvm_phase phase);
   if(!(uvm_config_db #(JtagMasterAgentConfig) :: get(this ,"","jtagMasterAgentConfig",jtagMasterAgentConfig)))
     `uvm_fatal(get_type_name(),"FAILED TO GET AGENT CONFIG IN MASTER")
 
+    $display("/n /n config is active is %s",jtagMasterAgentConfig.is_active);
+
   if(jtagMasterAgentConfig.is_active == UVM_ACTIVE) begin 
     $display("***************************************************************8CONNECTING DRIVER AND SEQUENCER");
     jtagMasterDriver = JtagMasterDriver :: type_id :: create("jtagMasterDriver",this);
