ee206c9fe950738bb05cfe0707483a35aaad3e49
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-04-14 00:07:00 +0530

    Update JtagTdiWidth16Test.sv

--- src/hvlTop/tb/JtagTdiWidth16Test.sv
+++ src/hvlTop/tb/JtagTdiWidth16Test.sv
@@ -1,31 +1,31 @@
-`ifndef JTAGWIDTH16TEST_INCLUDED_
-`define JTAGWIDTH16TEST_INCLUDED_
+`ifndef JTAGTDIWidth16TEST_INCLUDED_
+`define JTAGTDIWidth16TEST_INCLUDED_
 
-class JtagWidth16Test extends JtagBaseTest;
-  `uvm_component_utils(JtagWidth16Test)
+class JtagTdiWidth16Test extends JtagBaseTest;
+  `uvm_component_utils(JtagTdiWidth16Test)
 
-  extern function new(string name = "JtagWidth16Test" , uvm_component parent);
+  extern function new(string name = "JtagTdiWidth16Test" , uvm_component parent);
   extern virtual function void build_phase(uvm_phase phase);
   extern virtual task run_phase (uvm_phase phase);
-endclass : JtagWidth16Test
+endclass : JtagTdiWidth16Test
 
 
-function JtagWidth16Test :: new(string name = "JtagWidth16Test" , uvm_component parent);
+function JtagTdiWidth16Test :: new(string name = "JtagTdiWidth16Test" , uvm_component parent);
   super.new(name,parent);
 endfunction : new
 
 
-function void JtagWidth16Test :: build_phase(uvm_phase phase);
+function void JtagTdiWidth16Test :: build_phase(uvm_phase phase);
   super.build_phase(phase);
-  jtagEnvConfig.jtagMasterAgentConfig.jtagTestVectorWidth = testVectorWidth16Bit;
-  jtagEnvConfig.jtagMasterAgentConfig.jtagInstructionWidth = instructionWidth5Bit;
-   jtagEnvConfig.jtagSlaveAgentConfig.jtagTestVectorWidth = testVectorWidth16Bit;
-   jtagEnvConfig.jtagSlaveAgentConfig.jtagInstructionWidth = instructionWidth5Bit;
+  jtagEnvConfig.jtagMasterAgentConfig.jtagTestVectorTdiWidth = testVectorTdiWidth16Bit;
+  jtagEnvConfig.jtagMasterAgentConfig.jtagInstructionTdiWidth = instructionTdiWidth5Bit;
+   jtagEnvConfig.jtagSlaveAgentConfig.jtagTestVectorTdiWidth = testVectorTdiWidth16Bit;
+   jtagEnvConfig.jtagSlaveAgentConfig.jtagInstructionTdiWidth = instructionTdiWidth5Bit;
 endfunction : build_phase
 
 
 
-task  JtagWidth16Test :: run_phase(uvm_phase phase);
+task  JtagTdiWidth16Test :: run_phase(uvm_phase phase);
   jtagMasterTestingVirtualSequence = JtagMasterTestingVirtualSequence :: type_id :: create("JtagMasterTestingVirtualSequence");
   jtagMasterTestingVirtualSequence.setConfig(jtagEnvConfig.jtagMasterAgentConfig);
  
