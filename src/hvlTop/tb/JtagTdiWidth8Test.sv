b45abea2aa7ffcb1671bc49f4df151611d73bcd7
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-04-13 23:41:49 +0530

    Update JtagTdiWidth8Test.sv

--- src/hvlTop/tb/JtagTdiWidth8Test.sv
+++ src/hvlTop/tb/JtagTdiWidth8Test.sv
@@ -1,72 +1,31 @@
-`ifndef JTAGBASETEST_INCLUDED_
-`define JTAGBASETEST_INCLUDED_
+`ifndef JTAGWIDTH8TEST_INCLUDED_
+`define JTAGWIDTH8TEST_INCLUDED_
 
-class JtagBaseTest extends uvm_test;
-  `uvm_component_utils(JtagBaseTest)
+class JtagWidth8Test extends JtagBaseTest;
+  `uvm_component_utils(JtagWidth8Test)
 
-  JtagMasterTestingVirtualSequence jtagMasterTestingVirtualSequence;
-  JtagEnv jtagEnv;
-  JtagEnvConfig jtagEnvConfig;
-  
-  extern function new(string name = "JtagBaseTest" , uvm_component parent);
+  extern function new(string name = "JtagWidth8Test" , uvm_component parent);
   extern virtual function void build_phase(uvm_phase phase);
-  extern function void setupJtagEnvConfig();
-  extern function void setupJtagMasterAgentConfig();
-  extern function void setupJtagSlaveAgentConfig();
-  extern virtual function void end_of_elaboration_phase(uvm_phase phase);
   extern virtual task run_phase (uvm_phase phase);
-endclass : JtagBaseTest
+endclass : JtagWidth8Test
 
 
-function JtagBaseTest :: new(string name = "JtagBaseTest" , uvm_component parent);
+function JtagWidth8Test :: new(string name = "JtagWidth8Test" , uvm_component parent);
   super.new(name,parent);
 endfunction : new
 
 
-function void JtagBaseTest :: build_phase(uvm_phase phase);
+function void JtagWidth8Test :: build_phase(uvm_phase phase);
   super.build_phase(phase);
-  setupJtagEnvConfig();
-  jtagEnv = JtagEnv :: type_id :: create("jtagEnv",this);
+  jtagEnvConfig.jtagMasterAgentConfig.jtagTestVectorWidth = testVectorWidth8Bit;
+  jtagEnvConfig.jtagMasterAgentConfig.jtagInstructionWidth = instructionWidth5Bit;
+   jtagEnvConfig.jtagSlaveAgentConfig.jtagTestVectorWidth = testVectorWidth8Bit;
+   jtagEnvConfig.jtagSlaveAgentConfig.jtagInstructionWidth = instructionWidth5Bit;
 endfunction : build_phase
 
-function void  JtagBaseTest :: setupJtagEnvConfig();
-  jtagEnvConfig = JtagEnvConfig :: type_id :: create("jtagEnvConfig");
-  jtagEnvConfig.hasScoreboard = 1 ;
-  jtagEnvConfig.hasVirtualSequencer = 1;
-  uvm_config_db #(JtagEnvConfig) :: set(this,"*", "jtagEnvConfig",jtagEnvConfig);
-  setupJtagMasterAgentConfig();
-  setupJtagSlaveAgentConfig();
-endfunction : setupJtagEnvConfig
-
-function void  JtagBaseTest :: setupJtagMasterAgentConfig();
-  jtagEnvConfig.jtagMasterAgentConfig = JtagMasterAgentConfig :: type_id :: create("jtagMasterAgentConfig");
-  jtagEnvConfig.jtagMasterAgentConfig.hasCoverage =1;
-  jtagEnvConfig.jtagMasterAgentConfig.is_active = UVM_ACTIVE;
-  jtagEnvConfig.jtagMasterAgentConfig.jtagTestVectorWidth = testVectorWidth16Bit;
-  jtagEnvConfig.jtagMasterAgentConfig.jtagInstructionWidth = instructionWidth5Bit;
-  jtagEnvConfig.jtagMasterAgentConfig.jtagInstructionOpcode = bypassRegister;
 
 
-  uvm_config_db #(JtagMasterAgentConfig) :: set(null,"*", "jtagMasterAgentConfig",jtagEnvConfig.jtagMasterAgentConfig);
-
-endfunction : setupJtagMasterAgentConfig
-
-function void  JtagBaseTest :: setupJtagSlaveAgentConfig();
- jtagEnvConfig.jtagSlaveAgentConfig = JtagSlaveAgentConfig :: type_id :: create("jtagSlaveAgentConfig");
- jtagEnvConfig.jtagSlaveAgentConfig.hasCoverage =1;
- jtagEnvConfig.jtagSlaveAgentConfig.is_active = UVM_ACTIVE;
- jtagEnvConfig.jtagSlaveAgentConfig.jtagTestVectorWidth = testVectorWidth16Bit;
- jtagEnvConfig.jtagSlaveAgentConfig.jtagInstructionWidth = instructionWidth5Bit;
- jtagEnvConfig.jtagSlaveAgentConfig.jtagInstructionOpcode = boundaryScanRegisters;
-  uvm_config_db #(JtagSlaveAgentConfig) :: set(null,"*", "jtagSlaveAgentConfig",jtagEnvConfig.jtagSlaveAgentConfig);
-endfunction : setupJtagSlaveAgentConfig
-
-function void JtagBaseTest :: end_of_elaboration_phase(uvm_phase phase);
-  super.end_of_elaboration_phase(phase);
-  uvm_top.print_topology();
-endfunction :  end_of_elaboration_phase
-
-task  JtagBaseTest :: run_phase(uvm_phase phase);
+task  JtagWidth8Test :: run_phase(uvm_phase phase);
   jtagMasterTestingVirtualSequence = JtagMasterTestingVirtualSequence :: type_id :: create("JtagMasterTestingVirtualSequence");
   jtagMasterTestingVirtualSequence.setConfig(jtagEnvConfig.jtagMasterAgentConfig);
  
@@ -79,7 +38,3 @@ task  JtagBaseTest :: run_phase(uvm_phase phase);
 endtask : run_phase
 
 `endif
-
-
-
-  
