73f071210c815bf7f76f79f62af3c2d2f8e275c8
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-04-14 16:11:31 +0530

    Create JtagPatternBasedTest.sv

---
+++ src/hvlTop/tb/JtagPatternBasedTest.sv
@@ -0,0 +1,41 @@
+
+`ifndef JTAGPATTERNBASEDTEST_INCLUDED_
+`define JTAGPATTERNBASEDTEST_INCLUDED_
+
+class JtagPatternBasedTest extends JtagBaseTest;
+  `uvm_component_utils(JtagPatternBasedTest)
+
+  extern function new(string name = "JtagPatternBasedTest" , uvm_component parent);
+  extern virtual function void build_phase(uvm_phase phase);
+  extern virtual task run_phase (uvm_phase phase);
+endclass : JtagPatternBasedTest
+
+function JtagPatternBasedTest :: new(string name = "JtagPatternBasedTest" , uvm_component parent);
+  super.new(name,parent);
+endfunction : new
+
+
+function void JtagPatternBasedTest :: build_phase(uvm_phase phase);
+  super.build_phase(phase);
+  jtagEnvConfig.jtagMasterAgentConfig.jtagTestVectorWidth = testVectorWidth8Bit;
+  jtagEnvConfig.jtagMasterAgentConfig.jtagInstructionWidth = instructionWidth4Bit;
+   jtagEnvConfig.jtagSlaveAgentConfig.jtagTestVectorWidth = testVectorWidth8Bit;
+   jtagEnvConfig.jtagSlaveAgentConfig.jtagInstructionWidth = instructionWidth4Bit;
+   jtagEnvConfig.jtagMasterAgentConfig.patternNeeded = 8'b 10101010;
+endfunction : build_phase
+
+
+
+task JtagPatternBasedTest :: run_phase(uvm_phase phase);
+  jtagMasterPatternBasedVirtualSequence = JtagMastterPatternBasedVirtualSequence :: type_id :: create("JtagMasterPatternBasedVirtualSequence");
+  jtagMasterPatternBasedVirtualSequence.setConfig(jtagEnvConfig.jtagMasterAgentConfig);
+ 
+  phase.raise_objection(this);
+  repeat(2) begin 
+  jtagMasterPatternBasedVirtualSequence.start(jtagEnv.jtagVirtualSequencer);
+  end 
+  phase.drop_objection(this);
+
+endtask : run_phase
+
+`endif
