0f518b087d236dd87d3df32c335bfac66ca542d3
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-04-14 13:39:13 +0530

    Create JtagTdiWidth8BypassRegisterTest.sv

---
+++ src/hvlTop/tb/JtagTdiWidth8BypassRegisterTest.sv
@@ -0,0 +1,41 @@
+`ifndef JTAGTDIWIDTH8BYPASSREGISTERTEST_INCLUDED_
+`define JTAGTDIWIDTH8BYPASSREGISTERTEST_INCLUDED_
+
+class JtagTdiWidth8BypassRegisterTest extends JtagBaseTest;
+  `uvm_component_utils(JtagTdiWidth8BypassRegisterTest)
+
+  extern function new(string name = "JtagTdiWidth8BypassRegisterTest" , uvm_component parent);
+  extern virtual function void build_phase(uvm_phase phase);
+  extern virtual task run_phase (uvm_phase phase);
+endclass : JtagTdiWidth8BypassRegisterTest
+
+
+    function JtagTdiWidth8BypassRegisterTest :: new(string name = "JtagTdiWidth8BypassRegisterTest" , uvm_component parent);
+  super.new(name,parent);
+endfunction : new
+
+
+function void JtagTdiWidth8BypassRegisterTest :: build_phase(uvm_phase phase);
+  super.build_phase(phase);
+  jtagEnvConfig.jtagMasterAgentConfig.jtagTestVectorWidth = testVectorWidth8Bit;
+  jtagEnvConfig.jtagMasterAgentConfig.jtagInstructionWidth = instructionWidth5Bit;
+   jtagEnvConfig.jtagSlaveAgentConfig.jtagTestVectorWidth = testVectorWidth8Bit;
+   jtagEnvConfig.jtagSlaveAgentConfig.jtagInstructionWidth = instructionWidth5Bit;
+   jtagEnvConfig.jtagMasterAgentConfig.jtagInstructionOpcode = bypassRegister;
+endfunction : build_phase
+
+
+
+task  JtagTdiWidth8BypassRegisterTest :: run_phase(uvm_phase phase);
+  jtagMasterTestingVirtualSequence = JtagMasterTestingVirtualSequence :: type_id :: create("JtagMasterTestingVirtualSequence");
+  jtagMasterTestingVirtualSequence.setConfig(jtagEnvConfig.jtagMasterAgentConfig);
+ 
+  phase.raise_objection(this);
+  repeat(2) begin 
+  jtagMasterTestingVirtualSequence.start(jtagEnv.jtagVirtualSequencer);
+  end 
+  phase.drop_objection(this);
+
+endtask : run_phase
+
+`endif
