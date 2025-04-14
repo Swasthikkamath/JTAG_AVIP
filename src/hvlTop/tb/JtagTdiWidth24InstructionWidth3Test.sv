eece80469c0169ce4242cb8afee7d3ecbf157a43
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-04-14 12:10:40 +0530

    Update JtagTdiWidth24InstructionWidth3Test.sv

--- src/hvlTop/tb/JtagTdiWidth24InstructionWidth3Test.sv
+++ src/hvlTop/tb/JtagTdiWidth24InstructionWidth3Test.sv
@@ -3,14 +3,14 @@
 `define JTAGTDIWidth24INSTRUCTIONWIDTH3TEST_INCLUDED_
 
 class JtagTdiWidth24InstructionWidth3Test extends JtagBaseTest;
-  `uvm_component_utils(JtagTdiWidth24Test)
+  `uvm_component_utils(JtagTdiWidth24InstructionWidth3Test)
 
   extern function new(string name = "JtagTdiWidth24InstructionWidth3Test" , uvm_component parent);
   extern virtual function void build_phase(uvm_phase phase);
   extern virtual task run_phase (uvm_phase phase);
-endclass : JtagTdiWidth24Test
+endclass : JtagTdiWidth24InstructionWidth3Test
 
-function JtagTdiWidth24Test :: new(string name = "JtagTdiWidth24InstructionWidth3Test" , uvm_component parent);
+function JtagTdiWidth24InstructionWidth3Test :: new(string name = "JtagTdiWidth24InstructionWidth3Test" , uvm_component parent);
   super.new(name,parent);
 endfunction : new
 
