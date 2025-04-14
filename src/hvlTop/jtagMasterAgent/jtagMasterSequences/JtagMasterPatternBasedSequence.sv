969c045bd40c91babdb5eb713a07a6bef37232a8
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-04-14 15:56:25 +0530

    Create JtagMasterPatternBasedSequence.sv

---
+++ src/hvlTop/jtagMasterAgent/jtagMasterSequences/JtagMasterPatternBasedSequence.sv
@@ -0,0 +1,26 @@
+
+`ifndef JTAGMASTERPATTERNBASEDSEQUENCE_INCLUDED_
+`define JTAGMASTERPATTERNBASEDSEQUENCE_INCLUDED_
+
+class JtagMasterPatternBasedSequence extends JtagMasterBaseSequence;
+  `uvm_object_utils(JtagMasterPatternBasedSequence)
+  bit[31:0]patternNeeded
+  extern function new(string name = "JtagMasterPatternBasedSequence");
+  extern virtual task body();
+
+endclass : JtagMasterPatternBasedSequence 
+
+function JtagMasterPatternBasedSequence :: new(string name = "JtagMasterPatternBasedSequence");
+  super.new(name);
+endfunction : new
+
+task JtagMasterPatternBasedSequence :: body();
+  super.body();
+  req = JtagMasterTransaction :: type_id :: create("req");
+  req.randomize() with{jtagTestVector == patternNeeded;};
+  req.print();
+  start_item(req);
+  finish_item(req);
+endtask : body
+
+`endif
