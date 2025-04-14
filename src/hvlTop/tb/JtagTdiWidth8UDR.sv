9c12470f233f29fc2cf67f740af688398ef8ee66
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-04-14 13:48:48 +0530

    Create JtagTdiWidth8UDR.sv

---
+++ src/hvlTop/tb/JtagTdiWidth8UDR.sv
@@ -0,0 +1,43 @@
+
+
+`ifndef JTAGTDIWIDTH8UDR_INCLUDED_
+`define JTAGTDIWIDTH8UDR_INCLUDED_
+
+class JtagTdiWidth8UDR extends JtagBaseTest;
+  `uvm_component_utils(JtagTdiWidth8UDR)
+
+  extern function new(string name = "JtagTdiWidth8UDR" , uvm_component parent);
+  extern virtual function void build_phase(uvm_phase phase);
+  extern virtual task run_phase (uvm_phase phase);
+endclass : JtagTdiWidth8UDR
+
+
+    function JtagTdiWidth8UDR :: new(string name = "JtagTdiWidth8UDR" , uvm_component parent);
+  super.new(name,parent);
+endfunction : new
+
+
+function void JtagTdiWidth8UDR :: build_phase(uvm_phase phase);
+  super.build_phase(phase);
+  jtagEnvConfig.jtagMasterAgentConfig.jtagTestVectorWidth = testVectorWidth8Bit;
+  jtagEnvConfig.jtagMasterAgentConfig.jtagInstructionWidth = instructionWidth5Bit;
+   jtagEnvConfig.jtagSlaveAgentConfig.jtagTestVectorWidth = testVectorWidth8Bit;
+   jtagEnvConfig.jtagSlaveAgentConfig.jtagInstructionWidth = instructionWidth5Bit;
+   jtagEnvConfig.jtagMasterAgentConfig.jtagInstructionOpcode = userDefinedRegister;
+endfunction : build_phase
+
+
+
+task  JtagTdiWidth8UDR :: run_phase(uvm_phase phase);
+  jtagMasterTestingVirtualSequence = JtagMasterTestingVirtualSequence :: type_id :: create("JtagMasterTestingVirtualSequence");
+  jtagMasterTestingVirtualSequence.setConfig(jtagEnvConfig.jtagMasterAgentConfig);
+ 
+  phase.raise_objection(this);
+  repeat(2) begin 
+  jtagMasterTestingVirtualSequence.start(jtagEnv.jtagVirtualSequencer);
+  end 
+  phase.drop_objection(this);
+
+endtask : run_phase
+
+`endif
