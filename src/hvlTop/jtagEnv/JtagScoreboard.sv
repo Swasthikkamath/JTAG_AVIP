f36065895bcc7aa49d17e57934fc99149ea274bd
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-04-19 23:30:42 +0530

    Update JtagScoreboard.sv

--- src/hvlTop/jtagEnv/JtagScoreboard.sv
+++ src/hvlTop/jtagEnv/JtagScoreboard.sv
@@ -4,14 +4,14 @@
 class JtagScoreboard extends uvm_scoreboard;
  `uvm_component_utils(JtagScoreboard)
 
- uvm_analysis_export #(JtagMasterTransaction) jtagScoreboardMasterAnalysisExport;
- uvm_analysis_export #(JtagSlaveTransaction)  jtagScoreboardSlaveAnalysisExport;
+ uvm_analysis_export #(JtagControllerDeviceTransaction) jtagScoreboardControllerDeviceAnalysisExport;
+ uvm_analysis_export #(JtagTargetDeviceTransaction)  jtagScoreboardTargetDeviceAnalysisExport;
 
- uvm_tlm_analysis_fifo #(JtagMasterTransaction) jtagScoreboardMasterAnalysisFifo;
- uvm_tlm_analysis_fifo #(JtagSlaveTransaction) jtagScoreboardSlaveAnalysisFifo; 
+ uvm_tlm_analysis_fifo #(JtagControllerDeviceTransaction) jtagScoreboardControllerDeviceAnalysisFifo;
+ uvm_tlm_analysis_fifo #(JtagTargetDeviceTransaction) jtagScoreboardTargetDeviceAnalysisFifo; 
  
- JtagMasterTransaction jtagMasterTransaction;
- JtagSlaveTransaction jtagSlaveTransaction;
+ JtagControllerDeviceTransaction jtagControllerDeviceTransaction;
+ JtagTargetDeviceTransaction jtagTargetDeviceTransaction;
 
  extern function new(string name = "JtagScoreboard" , uvm_component parent);
  extern virtual function void build_phase(uvm_phase phase);
@@ -27,41 +27,41 @@ endfunction : new
 
 function void JtagScoreboard :: build_phase(uvm_phase phase);
   super.build_phase(phase);
-  jtagScoreboardMasterAnalysisExport = new("jtagScoreboardMasterAnalysisExport",this);
-  jtagScoreboardSlaveAnalysisExport = new("jtagScoreboardSlaveAnalysisExport",this);
-  jtagScoreboardMasterAnalysisFifo = new("jtagScoreboardMasterAnalysisFifo",this);
-  jtagScoreboardSlaveAnalysisFifo = new("jtagScoreboardSlaveAnalysisFifo",this);
+  jtagScoreboardControllerDeviceAnalysisExport = new("jtagScoreboardControllerDeviceAnalysisExport",this);
+  jtagScoreboardTargetDeviceAnalysisExport = new("jtagScoreboardTargetDeviceAnalysisExport",this);
+  jtagScoreboardControllerDeviceAnalysisFifo = new("jtagScoreboardControllerDeviceAnalysisFifo",this);
+  jtagScoreboardTargetDeviceAnalysisFifo = new("jtagScoreboardTargetDeviceAnalysisFifo",this);
  
 endfunction : build_phase
 
 function void JtagScoreboard :: connect_phase(uvm_phase phase);
   super.connect_phase(phase);
 
-  jtagScoreboardMasterAnalysisExport.connect(jtagScoreboardMasterAnalysisFifo.analysis_export);
-  jtagScoreboardSlaveAnalysisExport.connect(jtagScoreboardSlaveAnalysisFifo.analysis_export);
+  jtagScoreboardControllerDeviceAnalysisExport.connect(jtagScoreboardControllerDeviceAnalysisFifo.analysis_export);
+  jtagScoreboardTargetDeviceAnalysisExport.connect(jtagScoreboardTargetDeviceAnalysisFifo.analysis_export);
 
 endfunction:connect_phase
 
-task compareResult(JtagMasterTransaction jtagMasterTransaction,JtagSlaveTransaction jtagSlaveTransaction);
-  if((jtagMasterTransaction.jtagTestVector ===jtagSlaveTransaction.jtagTestVector)&& (jtagMasterTransaction.jtagInstruction===jtagSlaveTransaction.jtagInstruction))
+task compareResult(JtagControllerDeviceTransaction jtagControllerDeviceTransaction,JtagTargetDeviceTransaction jtagTargetDeviceTransaction);
+  if((jtagControllerDeviceTransaction.jtagTestVector ===jtagTargetDeviceTransaction.jtagTestVector)&& (jtagControllerDeviceTransaction.jtagInstruction===jtagTargetDeviceTransaction.jtagInstruction))
   begin 
-    `uvm_info("[ PASS ]",$sformatf("TDI = %b AND INSTRUCTION=%b",jtagMasterTransaction.jtagTestVector,jtagMasterTransaction.jtagInstruction),UVM_LOW);
+    `uvm_info("[ PASS ]",$sformatf("TDI = %b AND INSTRUCTION=%b",jtagControllerDeviceTransaction.jtagTestVector,jtagControllerDeviceTransaction.jtagInstruction),UVM_LOW);
   end 
   else begin 
-   `uvm_info("[ FAIL ]",$sformatf("TDI = %b AND INSTRUCTION=%b",jtagMasterTransaction.jtagTestVector,jtagMasterTransaction.jtagInstruction),UVM_LOW);
+   `uvm_info("[ FAIL ]",$sformatf("TDI = %b AND INSTRUCTION=%b",jtagControllerDeviceTransaction.jtagTestVector,jtagControllerDeviceTransaction.jtagInstruction),UVM_LOW);
   end 
 endtask 
 
 task JtagScoreboard :: run_phase(uvm_phase phase);
   super.run_phase(phase);
   forever begin 
-    jtagScoreboardMasterAnalysisFifo.get(jtagMasterTransaction);
-    `uvm_info("[MASTER TRANSACTION IN SCB]",$sformatf("THE INSTRUCTION IS %b and test vector is %b",jtagMasterTransaction.jtagInstruction,jtagMasterTransaction.jtagTestVector),UVM_LOW);
+    jtagScoreboardControllerDeviceAnalysisFifo.get(jtagControllerDeviceTransaction);
+    `uvm_info("[ControllerDevice TRANSACTION IN SCB]",$sformatf("THE INSTRUCTION IS %b and test vector is %b",jtagControllerDeviceTransaction.jtagInstruction,jtagControllerDeviceTransaction.jtagTestVector),UVM_LOW);
   
-  jtagScoreboardSlaveAnalysisFifo.get(jtagSlaveTransaction);
-   `uvm_info("[SLAVE TRANSACTION IN SCB]",$sformatf("THE INSTRUCTION IS %b and test vector is %b",jtagSlaveTransaction.jtagInstruction,jtagSlaveTransaction.jtagTestVector),UVM_LOW);
+  jtagScoreboardTargetDeviceAnalysisFifo.get(jtagTargetDeviceTransaction);
+   `uvm_info("[TargetDevice TRANSACTION IN SCB]",$sformatf("THE INSTRUCTION IS %b and test vector is %b",jtagTargetDeviceTransaction.jtagInstruction,jtagTargetDeviceTransaction.jtagTestVector),UVM_LOW);
 
-compareResult(jtagMasterTransaction,jtagSlaveTransaction);
+compareResult(jtagControllerDeviceTransaction,jtagTargetDeviceTransaction);
 
   end 
 endtask : run_phase 
